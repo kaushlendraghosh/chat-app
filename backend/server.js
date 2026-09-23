282d6d85bf3e779546da044b429147377b7beed0
kaushlendra <singhkaushlendra5002@gmail.com>
2026-09-24 00:57:03 +0530

    Add user listing functionality

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import cookieParser from "cookie-parser";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
 
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
